9f517fee0d10639aea74262af15135457a0b8452
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-14 08:14:51 -0600

    astro profile/pic

--- orgInit.sh
+++ orgInit.sh
@@ -4,9 +4,11 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n TrialAdmin
 sfdx force:user:create -f config/userDef/cloudy-user-def.json
 sfdx force:user:create -f config/userDef/codey-user-def.json
+sfdx force:user:create -f config/userDef/astro-user-def.json
 
-sfdx msm:user:photo -f assets/cloudy_lightning.png -l Cloudy
-sfdx msm:user:photo -f assets/codey_lightning.png  -l CodeBear
+sfdx msm:user:photo -f assets/cloudy-profile.png -l Cloudy
+sfdx msm:user:photo -f assets/codey-profile.png  -l CodeBear
+sfdx msm:user:photo -f assets/astro-profile.png  -l Nomical
 
 sfdx force:apex:execute -f SetupScripting/apexDeletions.cls
 sfdx force:data:tree:import -p data/masterImportPlan.json
