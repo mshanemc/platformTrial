035d9ae3a91272038ecb5101682532adc8aac30d
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-01-11 15:58:20 -0600

    faster init

--- orgInit.sh
+++ orgInit.sh
@@ -5,11 +5,6 @@ echo 'Pushing source to the scratch org'
 sfdx force:source:push
 
 sfdx force:user:permset:assign -n TrialAdmin
-sfdx force:user:permset:assign -n Referrals && sfdx force:data:bulk:upsert -f data/referrals.csv -s Referral__c -i Id &
-
-sfdx force:user:create -f config/userDef/cloudy-user-def.json && sfdx msm:user:photo -f assets/cloudy-profile.png -l Cloudy &
-sfdx force:user:create -f config/userDef/codey-user-def.json && sfdx msm:user:photo -f assets/codey-profile.png  -l CodeBear &
-sfdx force:user:create -f config/userDef/astro-user-def.json && sfdx msm:user:photo -f assets/astro-profile.png  -l Nomical &
 
 sfdx msm:user:password:set -l User -g User -p sfdx1234
 sfdx force:org:open -p one/one.app#/n/Start
@@ -18,3 +13,8 @@ sfdx force:apex:execute -f SetupScripting/apexDeletions.cls
 sfdx force:data:tree:import -p data/masterImportPlan.json
 sfdx force:apex:execute -f SetupScripting/urlSetup.cls &
 sfdx force:apex:execute -f SetupScripting/dateAdjustments.cls &
+sfdx force:user:permset:assign -n Referrals && sfdx force:data:bulk:upsert -f data/referrals.csv -s Referral__c -i Id &
+
+sfdx force:user:create -f config/userDef/cloudy-user-def.json && sfdx msm:user:photo -f assets/cloudy-profile.png -l Cloudy &
+sfdx force:user:create -f config/userDef/codey-user-def.json && sfdx msm:user:photo -f assets/codey-profile.png  -l CodeBear &
+sfdx force:user:create -f config/userDef/astro-user-def.json && sfdx msm:user:photo -f assets/astro-profile.png  -l Nomical &
