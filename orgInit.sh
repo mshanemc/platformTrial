31f31b8974b031a6910a454f3b476770a490be4f
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-02-02 12:00:48 -0600

    add recruiting in zipped format

--- orgInit.sh
+++ orgInit.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
-echo 'Creating a scratch org'
 sfdx force:org:create -s -f config/project-scratch-def.json -d 1
-echo 'Pushing source to the scratch org'
 sfdx force:source:push
 
 sfdx force:user:permset:assign -n TrialAdmin
-
-sfdx msm:user:password:set -l User -g User -p sfdx1234
-sfdx force:org:open -p one/one.app#/n/Start
-
 sfdx force:apex:execute -f SetupScripting/apexDeletions.cls
 sfdx force:data:tree:import -p data/masterImportPlan.json
+sfdx force:org:open -p one/one.app#/n/Start
+
 sfdx force:apex:execute -f SetupScripting/urlSetup.cls &
 sfdx force:apex:execute -f SetupScripting/dateAdjustments.cls &
+
+sfdx force:mdapi:deploy -f zippedMDAPI/recruiting.zip -w 20
 sfdx force:user:permset:assign -n Referrals && sfdx force:data:bulk:upsert -f data/referrals.csv -s Referral__c -i Id &
 
 sfdx force:user:create -f config/userDef/cloudy-user-def.json && sfdx msm:user:photo -f assets/cloudy-profile.png -l Cloudy &
