9e8ca696c92c990f73e5b1f1ed76300c613bf710
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-12-20 09:51:12 -0600

    rewrite the orgInit (simplify) since pools now help

--- orgInit.sh
+++ orgInit.sh
@@ -5,12 +5,18 @@ sfdx force:user:permset:assign -n TrialAdmin
 # sfdx force:apex:execute -f SetupScripting/apexDeletions.cls
 sfdx force:data:tree:import -p data/masterImportPlan.json
 
-sfdx force:apex:execute -f SetupScripting/urlSetup.cls &
+sfdx force:apex:execute -f SetupScripting/urlSetup.cls
 sfdx force:org:open -p one/one.app#/n/Start
 
-sleep 0.1 && sfdx force:user:create -f config/userDef/cloudy-user-def.json && sfdx shane:user:photo -f assets/cloudy-profile.png -l Cloudy &
-sleep 0.1 && sfdx force:user:create -f config/userDef/codey-user-def.json && sfdx shane:user:photo -f assets/codey-profile.png  -l CodeBear &
-sleep 0.1 && sfdx force:user:create -f config/userDef/astro-user-def.json && sfdx shane:user:photo -f assets/astro-profile.png  -l Nomical &
+sleep 0.1
+sfdx force:user:create -f config/userDef/cloudy-user-def.json
+sfdx shane:user:photo -f assets/cloudy-profile.png -l Cloudy
+sleep 0.1
+sfdx force:user:create -f config/userDef/codey-user-def.json
+sfdx shane:user:photo -f assets/codey-profile.png  -l CodeBear
+sleep 0.1
+sfdx force:user:create -f config/userDef/astro-user-def.json
+sfdx shane:user:photo -f assets/astro-profile.png  -l Nomical
 
 # install packages
 
@@ -18,8 +24,9 @@ sleep 0.1 && sfdx force:user:create -f config/userDef/astro-user-def.json && sfd
 sfdx force:package:install --package 04t6A000001Uee5QAC -w 20
 sfdx force:user:permset:assign -n VolunteeringApp
 sfdx force:data:tree:import -p data/volunteeringApp/VolunteerImportPlan.json
-sfdx force:apex:execute -f SetupScripting/VolunteerSetupScript.cls &
+sfdx force:apex:execute -f SetupScripting/VolunteerSetupScript.cls
 
 # recruiting
 sfdx force:package:install --package 04t6A000001Uee0QAC -w 20
-sfdx force:user:permset:assign -n Referrals && sfdx force:data:bulk:upsert -f data/referrals.csv -s Referral__c -i Id &
+sfdx force:user:permset:assign -n Referrals
+sfdx force:data:bulk:upsert -f data/referrals.csv -s Referral__c -i Id
