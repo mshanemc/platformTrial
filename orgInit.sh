b3b6408ff74577914caa4f3f27512401c6a1177b
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-01 18:09:29 -0500

    remove sleeps from secondary user creation

--- orgInit.sh
+++ orgInit.sh
@@ -8,13 +8,10 @@ sfdx force:data:tree:import -p data/masterImportPlan.json
 sfdx force:apex:execute -f SetupScripting/urlSetup.cls
 sfdx force:org:open -p one/one.app#/n/Start
 
-sleep 0.1
 sfdx force:user:create -f config/userDef/cloudy-user-def.json
 sfdx shane:user:photo -f assets/cloudy-profile.png -l Cloudy
-sleep 0.1
 sfdx force:user:create -f config/userDef/codey-user-def.json
 sfdx shane:user:photo -f assets/codey-profile.png  -l CodeBear
-sleep 0.1
 sfdx force:user:create -f config/userDef/astro-user-def.json
 sfdx shane:user:photo -f assets/astro-profile.png  -l Nomical
 
